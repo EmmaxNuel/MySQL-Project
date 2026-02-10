7abadb34f878de2e7b88628fc50b9cef5f4d2e91
Emmax <olatunjie355@gmail.com>
2026-02-10 09:53:23 +0100

    FIRST PROJECT😊😊😊😊😊😊😊

--- Chapter one/Classtask/E commerce project.sql
+++ Chapter one/Classtask/E commerce project.sql
@@ -1,7 +1,16 @@
 CREATE DATABASE user_db;
 CREATE TABLE users( 
-	id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
+	user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(100),
     email VARCHAR(100) UNIQUE);
-CREATE TABLE products;
-CREATE TABLE orders;
+    
+CREATE TABLE products(
+	product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100)
+    );
+CREATE TABLE orders(
+	order_id SERIAL PRIMARY KEY,
+    user_id SERIAL NOT NULL,
+    product_id SERIAL NOT NULL,
+    FOREIGN KEY(id) REFERENCES User(user_id)
+    );
