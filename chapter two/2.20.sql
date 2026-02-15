e75681b1fad7f6e55250db3de2dbe79f0dfdf351
Emmax <olatunjie355@gmail.com>
2026-02-15 08:16:58 +0100

    My SQL chapter 2

---
+++ chapter two/2.20.sql
@@ -0,0 +1,8 @@
+-- Write an SQL statement to display unique WarehouseIDs.
+USE cape_codd;
+
+select *
+from warehouse;
+
+select  warehouseID
+from warehouse;
---
+++ chapter two/2.21.sql
@@ -0,0 +1,6 @@
+-- Write an SQL statement to display all of the columns without using the SQL asterisk wildcard character
+
+use cape_codd;
+
+select WarehouseID,SKU,SKU_Description,QuantityOnHand,QuantityOnOrder
+from inventory;
